317d249ded5192a85c674c4576647f09eb32ba81
unknown <9davidmuia@gmail.com>
2020-11-10 23:50:49 +0300

    todo

---
+++ todo/src/dashboard/dash.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './dash.css'
+import  SideBar  from './sidebar';
+export default class Dashboard extends React.Component {
+
+    render() {
+        return (<div className='body'>
+ <SideBar /> 
+
+        </div>)
+    }
+}
